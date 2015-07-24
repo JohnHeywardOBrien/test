260e3d1ef0707a2bfb569b31fb6cae8296aaee44
John O'Brien <reachjohnnow@gmail.com>
2015-07-24 17:03:03 +0000

    Add do/end loop to check if Directories exist
    Script will now loop through DIR_NAME array and check if directories
    exist with the names in the array.
    
    If they do exist, the script will stop with an abort message
    If not, it will carry on, create the specified folders and files, and
    copy(mv)/rename them

--- script.rb
+++ script.rb
@@ -12,25 +12,30 @@ require './Models/RandomJson'
 require './Models/RandomFileName'
 require './Models/FileNametoGUID'
 
-directory_name = %w[Original Modified]
-Dir.mkdir(directory_name[0]) unless File.exists?(directory_name[0])
-Dir.mkdir(directory_name[1]) unless File.exists?(directory_name[1])
-
-# thought aboutusing method#to_proc for this. I was stumbling around with the best 
-# way to do this, and was lead down the road to learing about to_proc
-# but I'm not sure how to use unless with it... 
-# %w[Original Modified].each(&Dir.method(:mkdir)) #unless File.exists?(%w[Original Modified])
 
 
-# loop to create files within /Original folder
-1.upto(2) do |i|
-  File.open("Original/#{RandomFileName.new.filename.to_s}.json", "w+") do |file|
-    file.write("#{RandomJson.new.to_json}")
+DIR_NAMES = %w[Original Modified]
+DIR_NAMES.each do |dname|
+  if File.exists?(dname)
+    abort("The specified folders already exist. 
+           No further action will be taken.")
+  else
+    Dir.mkdir(dname)
+    puts "Starting creation of folders"
   end
 end
 
 
-#move and rename
+# loop to create files within Original folder
+1.upto(20) do |i|
+  File.open("Original/#{RandomFileName.new.filename.to_s}.json", "w+") do |file|
+    file.write("#{RandomJson.new.to_json}")
+    puts 
+  end
+end
+
+
+# move and rename
 files = Dir["Original/*.json"].collect{|f| File.expand_path(f)}
   files.each_with_index do |file, index|
     puts "copying file #{index}"
